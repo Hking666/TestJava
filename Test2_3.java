6b9903c82cc43eae4fed9f7e477cf6dffcecf6b0
Hking666 <49830689+Hking666@users.noreply.github.com>
2019-04-29 21:07:31 +0800

    Test2_3.java
    将英尺转换位米

---
+++ Test2_3.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Test2_3 {
+	public static void main(String[] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a value for feet : ");
+		
+		double feet = input.nextDouble();
+		
+		double meters = feet * 0.305 ; 
+		
+		System.out.println( feet + " feet is " + meters + " meters ");
+		
+	}
+}
